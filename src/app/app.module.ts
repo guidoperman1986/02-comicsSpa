d9afc1005770ffc1563bb12730bc854722ef4cd0
Guido <guidoperman@gmail.com>
2018-06-14 14:03:00 -0300

    Heroes listos y rutas listas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
 
+//Rutas
+import { APP_ROUTING } from "./app.routes";
+
+//Services
+import { HeroesService } from "./services/heroes.service";
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
